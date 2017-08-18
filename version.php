0f4a0ae1e0583dc575302acb2b53d389fe159be6
Eduardo Kraus - Aulas <eduardokraus@MacNovo-do-kraus.local>
2017-08-18 10:48:21 -0300

    Testar se as configurações do SMTP estão corretas.

--- version.php
+++ version.php
@@ -21,7 +21,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2017081601; // Version
+$plugin->version = 2017081800; // Version
 $plugin->requires = 2011120500;
 $plugin->release = '1.1.2';
 $plugin->maturity = MATURITY_RC;
