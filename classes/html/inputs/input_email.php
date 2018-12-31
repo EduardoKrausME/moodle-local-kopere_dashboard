c444b7872f77dd64b3ee06fddee55d62f029e1d3
Eduardo Kraus <kraus@eduardokraus.com>
2018-12-30 23:46:08 -0200

    add input_email

---
+++ classes/html/inputs/input_email.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @created    30/12/18 23:45
+ * @package    local_kopere_dashboard
+ * @copyright  2017 Eduardo Kraus {@link http://eduardokraus.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_kopere_dashboard\html\inputs;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class input_email
+ *
+ * @package local_kopere_dashboard\html\inputs
+ */
+class input_email extends input_base {
+    /**
+     * input_email constructor.
+     */
+    public function __construct() {
+        $this->set_type('email');
+    }
+
+    /**
+     * @return input_email
+     */
+    public static function new_instance() {
+        return new input_email();
+    }
+}
