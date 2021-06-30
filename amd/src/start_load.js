a40a813e7963d1ae4160222b08a0ec7afe613274
Eduardo Kraus <kraus@eduardokraus.com>
2021-06-30 12:02:18 -0300

    Update start_load.js

--- amd/src/start_load.js
+++ amd/src/start_load.js
@@ -3,7 +3,7 @@ define([
     "local_kopere_dashboard/maskedinput",
     "local_kopere_dashboard/validate",
     "local_kopere_dashboard/iosCheckbox",
-], function($, daterangepicker, validator) {
+], function($, maskedinput, validate, iosCheckbox) {
     return {
         init : function() {
             $(".ios-checkbox").iosCheckbox();
