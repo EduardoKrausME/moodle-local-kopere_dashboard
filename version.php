0f60b8503651fbae2a2deaf0dd9793707b0c022b
Eduardo Kraus <kraus@eduardokraus.com>
2023-11-14 11:58:15 -0300

    V 2.5.25

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023090400; // ..
+$plugin->version = 2023111400; // ..
 $plugin->requires = 2015111610; // Moodle 3.0.
-$plugin->release = '2.5.24';
+$plugin->release = '2.5.25';
 $plugin->maturity = MATURITY_RC;
 $plugin->component = 'local_kopere_dashboard';
