39c71c064ea531b0a192a50330c3f21eed28035a
Eduardo Kraus <kraus@eduardokraus.com>
2023-12-15 20:04:13 -0300

    WebPages foi reformulado

---
+++ classes/util/webpages_util.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * User: Eduardo Kraus
+ * Date: 15/12/2023
+ * Time: 12:43
+ */
+
+namespace local_kopere_dashboard\util;
+
+
+use moodle_url;
+
+class webpages_util {
+    /**
+     * @param $printtext
+     * @throws \dml_exception
+     * @throws \coding_exception
+     * @throws \moodle_exception
+     */
+    public function notfound($printtext) {
+        global $PAGE, $OUTPUT, $CFG;
+
+        header('HTTP/1.0 404 Not Found');
+        $PAGE->set_context(\context_system::instance());
+        $PAGE->set_pagelayout(get_config('local_kopere_dashboard', 'webpages_theme'));
+        $PAGE->set_title(get_string('error'));
+        $PAGE->set_heading(get_string('error'));
+
+        $PAGE->navbar->add(get_string_kopere('webpages_allpages'), new moodle_url("/local/kopere_dashboard/"));
+        $PAGE->navbar->add(get_string('error'));
+
+        echo $OUTPUT->header();
+
+        echo "<div class='element-box text-center page404'>
+                  <h2>" . get_string('error') . "</h2>
+                  <div>" . get_string_kopere($printtext) . "</div>
+                  <p><a href='{$CFG->wwwroot}/local/kopere_dashboard/'>" . get_string_kopere('webpages_allpages') . "</a></p>
+              </div>";
+
+        echo $OUTPUT->footer();
+        die();
+    }
+
+    /**
+     * @throws \dml_exception
+     */
+    function analytics() {
+        global $OUTPUT;
+
+        $webpagesanalyticsid = get_config('local_kopere_dashboard', 'webpages_analytics_id');
+        if (strlen($webpagesanalyticsid) > 5 && strlen($webpagesanalyticsid) < 15) {
+
+            $data = [
+                'webpagesanalyticsid' => $webpagesanalyticsid
+            ];
+            echo $OUTPUT->render_from_template('local_kopere_dashboard/google_analytics', $data);
+        }
+    }
+}
--- index.php
+++ index.php
@@ -43,7 +43,7 @@ if ($pagelink) {
 
     if ($webpages == null) {
         $PAGE->set_url(new moodle_url("/local/kopere_dashboard/"));
-        notfound("webpages_error_page");
+        \local_kopere_dashboard\util\webpages_util::notfound("webpages_error_page");
     }
 
     $PAGE->set_url(new moodle_url("/local/kopere_dashboard/?p={$pagelink}"));
@@ -77,10 +77,28 @@ if ($pagelink) {
         }
     }
     echo '<div class="container">';
+
+    preg_match_all('/\[\[(kopere_\w+)::(\w+)(->|-&gt;)(\w+)\((.*?)\)]]/', $webpages->text, $classes);
+    foreach ($classes[0] as $key => $replace) {
+        $classname = $classes[1][$key];
+        $function = $classes[2][$key];
+        $metodo = $classes[4][$key];
+        $parametro = $classes[5][$key];
+        $class = "\\local_{$classname}\\{$function}";
+
+        if (class_exists($class)) {
+            if (method_exists($class, $metodo)) {
+                $newreplace = $class::$metodo($parametro);
+
+                $webpages->text = str_replace($replace, $newreplace, $webpages->text);
+            }
+        }
+    }
     echo $webpages->text;
+
     echo '</div>';
 
-    analytics();
+    \local_kopere_dashboard\util\webpages_util::analytics();
     echo $OUTPUT->footer();
 } else {
 
@@ -88,7 +106,7 @@ if ($pagelink) {
         /** @var \local_kopere_dashboard\vo\kopere_dashboard_menu $menu */
         $menu = $DB->get_record('kopere_dashboard_menu', array('link' => $menulink));
         if ($menu == null) {
-            notfound("webpages_error_menu");
+            \local_kopere_dashboard\util\webpages_util::notfound("webpages_error_menu");
         }
 
         $PAGE->set_url(new moodle_url("/local/kopere_dashboard/?menu={$menu->link}"));
@@ -112,16 +130,16 @@ if ($pagelink) {
     }
     echo $OUTPUT->header();
 
-    echo '<div class="courses frontpage-course-list-all">';
-    echo '<div class="container">';
+    $data = ["menus" => []];
 
     /** @var \local_kopere_dashboard\vo\kopere_dashboard_menu $menu */
     foreach ($menus as $menu) {
-
         if (!$menulink) {
-            echo "<h3><a href='{$CFG->wwwroot}/local/kopere_dashboard/?menu={$menu->link}'>{$menu->title}</a></h1>";
+            $menu->menulink = [
+                'link' => $menu->link,
+                'title' => $menu->title
+            ];
         }
-        echo '<div class="row">';
 
         $sql = "SELECT * FROM {kopere_dashboard_webpages} WHERE visible = 1 ORDER BY pageorder ASC";
         $webpagess = $DB->get_records_sql($sql);
@@ -129,101 +147,39 @@ if ($pagelink) {
         /** @var \local_kopere_dashboard\vo\kopere_dashboard_webpages $webpages */
         foreach ($webpagess as $webpages) {
 
-            $valor = "";
+            $webpages->link = "{$CFG->wwwroot}/local/kopere_dashboard/?p={$webpages->link}";
+
             if (file_exists(__DIR__ . "/../kopere_pay/lib.php") && $webpages->courseid) {
                 $kopere_pay_detalhe = $DB->get_record('kopere_pay_detalhe', array('course' => $webpages->courseid));
-                $number = str_replace(".", "", $kopere_pay_detalhe->preco);
-                $number = str_replace(",", ".", $number);
-                $number = floatval("0{$number}");
+                $precoInt = str_replace(".", "", $kopere_pay_detalhe->preco);
+                $precoInt = str_replace(",", ".", $precoInt);
+                $precoInt = floatval("0{$precoInt}");
 
-                if (!$number) {
-                    $valor = "<div class='preco-curso preco-curso-free'>Gratis</div>";
+                if (!$precoInt) {
+                    $webpages->cursofree = true;
                 } else {
-                    $valor = "<div class='preco-curso'>R$ {$kopere_pay_detalhe->preco}</div>";
+                    $webpages->cursopago = true;
+                    $webpages->cursopreco = $kopere_pay_detalhe->preco;
                 }
             }
 
-            $imagem = "";
             $fs = get_file_storage();
             $file = $fs->get_file(context_system::instance()->id, 'local_kopere_dashboard', 'webpage_image', $webpages->id, '/', 'webpage_image.img');
             if ($file && isset($file->get_filename()[3])) {
-                $imagem = moodle_url::make_pluginfile_url($file->get_contextid(), $file->get_component(), $file->get_filearea(), $file->get_itemid(), "/", $file->get_filename());
+                $webpages->imagem = moodle_url::make_pluginfile_url($file->get_contextid(), $file->get_component(), $file->get_filearea(), $file->get_itemid(), "/", $file->get_filename());
+            } else {
+                $webpages->imagem = $OUTPUT->image_url("course-default", "local_kopere_dashboard")->out(false);
             }
+            $webpages->text = \local_kopere_dashboard\util\html::truncate_text(strip_tags($webpages->text), 300);
 
-            echo "
-                <div class='col-lg-4 col-md-6 col-sm-12'>
-                    <div class='box-page'>
-                        <div class='thumbnail'>
-                            <a href='{$CFG->wwwroot}/local/kopere_dashboard/?p={$webpages->link}'>
-                                <span style='background-image:url({$imagem})'></span>
-                            </a>
-                            {$valor}
-                        </div>
-                        <div class='content'>
-                            <h5 class='title'>
-                                <a href='{$CFG->wwwroot}/local/kopere_dashboard/?p={$webpages->link}'>{$webpages->title}</a>
-                            </h5>
-                            <p>" . \local_kopere_dashboard\util\html::truncate_text(strip_tags($webpages->text), 300) . "</p>
-                        </div>
-                    </div>
-                </div>";
+            if (!isset($menu->webpages)) $menu->webpages = [];
+            $menu->webpages[] = $webpages;
         }
-        echo '</div>';
+        $data['menus'][] = $menu;
     }
-    echo '</div>';
-    echo '</div>';
 
-    analytics();
+    echo $OUTPUT->render_from_template('local_kopere_dashboard/index_webpages', $data);
+
+    \local_kopere_dashboard\util\webpages_util::analytics();
     echo $OUTPUT->footer();
-}
-
-function analytics() {
-    $webpagesanalyticsid = get_config('local_kopere_dashboard', 'webpages_analytics_id');
-    if (strlen($webpagesanalyticsid) > 5 && strlen($webpagesanalyticsid) < 15) {
-        echo "
-            <script>
-                (function(i,s,o,g,r,a,m){
-                    i['GoogleAnalyticsObject']=r;
-                    i[r]=i[r]||function(){
-                        (i[r].q=i[r].q||[]).push(arguments)
-                    },i[r].l=1*new Date();
-                    a=s.createElement(o), m=s.getElementsByTagName(o)[0];
-                    a.async=1;
-                    a.src=g;
-                    m.parentNode.insertBefore(a,m)
-                })(window,document,'script','https:// www.google-analytics.com/analytics.js','ga');
-                ga('create', '{$webpagesanalyticsid}', 'auto');
-                ga('send', 'pageview');
-            </script>";
-    }
-}
-
-/**
- * @param $printtext
- * @throws coding_exception
- * @throws dml_exception
- * @throws moodle_exception
- */
-function notfound($printtext) {
-    global $PAGE, $OUTPUT, $CFG;
-
-    header('HTTP/1.0 404 Not Found');
-    $PAGE->set_context(\context_system::instance());
-    $PAGE->set_pagelayout(get_config('local_kopere_dashboard', 'webpages_theme'));
-    $PAGE->set_title(get_string('error'));
-    $PAGE->set_heading(get_string('error'));
-
-    $PAGE->navbar->add(get_string_kopere('webpages_allpages'), new moodle_url("/local/kopere_dashboard/"));
-    $PAGE->navbar->add(get_string('error'));
-
-    echo $OUTPUT->header();
-
-    echo "<div class='element-box text-center page404'>
-                  <h2>" . get_string('error') . "</h2>
-                  <div>" . get_string_kopere($printtext) . "</div>
-                  <p><a href='{$CFG->wwwroot}/local/kopere_dashboard/'>" . get_string_kopere('webpages_allpages') . "</a></p>
-              </div>";
-
-    echo $OUTPUT->footer();
-    die();
 }
