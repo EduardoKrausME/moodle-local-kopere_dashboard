38e9b34caad0b6320a7b2d844d529a98b0a456fe
Eduardo Kraus <kraus@eduardokraus.com>
2024-05-08 10:27:32 -0300

    V 2.6.5

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024050700;
+$plugin->version = 2024050800;
 $plugin->requires = 2015111610;
 $plugin->release = '2.6.5';
 $plugin->maturity = MATURITY_RC;
