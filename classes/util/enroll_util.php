78962930d905a900f6e15ce06732e845fc6ffa8b
Eduardo Kraus <kraus@eduardokraus.com>
2024-09-19 14:41:45 -0300

    BUG Enrol

--- classes/util/enroll_util.php
+++ classes/util/enroll_util.php
@@ -151,12 +151,18 @@ class enroll_util {
      *      7 => user
      *      8 => frontpage
      *
+     * @param \stdClass $course
+     * @param \stdClass $user
+     * @param int $timestart
+     * @param int $timeend
+     * @param int $roleid
+     *
      * @return bool
      *
-     * @throws \dml_exception
      * @throws \coding_exception
+     * @throws \dml_exception
      */
-    public static function enrol($course, $user, $timestart, $timeend, $roleid = 5) {
+    public static function enrol($course, $user, $timestart = 0, $timeend = 0, $roleid = 5) {
         global $DB, $PAGE, $CFG;
 
         $enrol = $DB->get_record('enrol', ['courseid' => $course->id, 'enrol' => 'manual']);
@@ -179,9 +185,9 @@ class enroll_util {
         /** @var \enrol_manual_plugin $plugin */
         $plugin = $plugins[$instance->enrol];
         if ($plugin->allow_enrol($instance)) {
-            $roleid = 5;
-            $timestart = time();
-            $timeend = 0;
+            if ($timestart == 0) {
+                $timestart = time();
+            }
             $recovergrades = 0;
             $plugin->enrol_user($instance, $user->id, $roleid, $timestart, $timeend, null, $recovergrades);
         } else {
