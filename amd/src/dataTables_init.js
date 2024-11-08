becb80e92dae0239c1006fa3a918091605e98fc2
Eduardo Kraus <kraus@eduardokraus.com>
2024-11-08 15:08:55 -0300

    Update dataTables_init.js

--- amd/src/dataTables_init.js
+++ amd/src/dataTables_init.js
@@ -92,7 +92,7 @@ define([
                     var month = twoDigit(a.getMonth() + 1);
                     var day = twoDigit(a.getDate());
 
-                    var result = M.util.get_string('date_renderer_format', 'local_kopere_dashboard');
+                    var result = M.util.get_string('date_renderer_format', "local_kopere_dashboard");
                     result = result.replace("day", day);
                     result = result.replace("month", month);
                     result = result.replace("year", year);
@@ -122,7 +122,7 @@ define([
                     var hour = twoDigit(a.getHours());
                     var min = twoDigit(a.getMinutes());
 
-                    var result = M.util.get_string('datetime_renderer_format', 'local_kopere_dashboard');
+                    var result = M.util.get_string('datetime_renderer_format', "local_kopere_dashboard");
                     result = result.replace("day", day);
                     result = result.replace("month", month);
                     result = result.replace("year", year);
@@ -135,9 +135,9 @@ define([
                     if (type != 'display') {
                         if (type == 'filter') {
                             if (!data) {
-                                return M.util.get_string('invisible', 'local_kopere_dashboard');
+                                return M.util.get_string('invisible', "local_kopere_dashboard");
                             } else {
-                                return M.util.get_string('visible', 'local_kopere_dashboard');
+                                return M.util.get_string('visible', "local_kopere_dashboard");
                             }
                         }
                         return data;
@@ -145,10 +145,10 @@ define([
 
                     if (!data) {
                         return '<div class="status-pill grey"  title="' +
-                            M.util.get_string('invisible', 'local_kopere_dashboard') +
+                            M.util.get_string('invisible', "local_kopere_dashboard") +
                             '"></div>';
                     } else {
-                        return '<div class="status-pill green" title="' + M.util.get_string('visible', 'local_kopere_dashboard') +
+                        return '<div class="status-pill green" title="' + M.util.get_string('visible', "local_kopere_dashboard") +
                             '"></div>';
                     }
                 },
@@ -156,9 +156,9 @@ define([
                     if (type != 'display') {
                         if (type == 'filter') {
                             if (data) {
-                                return M.util.get_string('inactive', 'local_kopere_dashboard');
+                                return M.util.get_string('inactive', "local_kopere_dashboard");
                             } else {
-                                return M.util.get_string('active', 'local_kopere_dashboard');
+                                return M.util.get_string('active', "local_kopere_dashboard");
                             }
                         }
                         return data;
@@ -166,11 +166,11 @@ define([
 
                     if (data) {
                         return '<div class="status-pill grey"  title="' +
-                            M.util.get_string('inactive', 'local_kopere_dashboard') +
+                            M.util.get_string('inactive', "local_kopere_dashboard") +
                             '"></div>';
                     } else {
                         return '<div class="status-pill green" title="' +
-                            M.util.get_string('active', 'local_kopere_dashboard') +
+                            M.util.get_string('active', "local_kopere_dashboard") +
                             '"></div>';
                     }
                 },
@@ -178,9 +178,9 @@ define([
                     if (type != 'display') {
                         if (type == 'filter') {
                             if (!data) {
-                                return M.util.get_string('notification_status_deleted', 'local_kopere_dashboard');
+                                return M.util.get_string('notification_status_deleted', "local_kopere_dashboard");
                             } else {
-                                return M.util.get_string('active', 'local_kopere_dashboard');
+                                return M.util.get_string('active', "local_kopere_dashboard");
                             }
                         }
                         return data;
@@ -188,10 +188,10 @@ define([
 
                     if (!data) {
                         return `<div class="status-pill grey"
-                                     title="${M.util.get_string('notification_status_deleted', 'local_kopere_dashboard')}"></div>`;
+                                     title="${M.util.get_string('notification_status_deleted', "local_kopere_dashboard")}"></div>`;
                     } else {
                         return `<div class="status-pill green"
-                                     title="${M.util.get_string('active', 'local_kopere_dashboard')}"></div>`;
+                                     title="${M.util.get_string('active', "local_kopere_dashboard")}"></div>`;
                     }
                 },
                 trueFalseRenderer : function(data, type, row, info) {
@@ -326,48 +326,48 @@ define([
 
                 params.columnDefs = newColumnDefs;
                 params.oLanguage = {
-                    sEmptyTable        : M.util.get_string('datatables_sEmptyTable', 'local_kopere_dashboard'),
-                    sInfo              : M.util.get_string('datatables_sInfo', 'local_kopere_dashboard'),
-                    sInfoEmpty         : M.util.get_string('datatables_sInfoEmpty', 'local_kopere_dashboard'),
-                    sInfoFiltered      : M.util.get_string('datatables_sInfoFiltered', 'local_kopere_dashboard'),
-                    sInfoPostFix       : M.util.get_string('datatables_sInfoPostFix', 'local_kopere_dashboard'),
-                    sInfoThousands     : M.util.get_string('datatables_sInfoThousands', 'local_kopere_dashboard'),
-                    sLengthMenu        : M.util.get_string('datatables_sLengthMenu', 'local_kopere_dashboard'),
-                    sLoadingRecords    : M.util.get_string('datatables_sLoadingRecords', 'local_kopere_dashboard'),
-                    sProcessing        : M.util.get_string('datatables_sProcessing', 'local_kopere_dashboard'),
-                    sErrorMessage      : M.util.get_string('datatables_sErrorMessage', 'local_kopere_dashboard'),
-                    sZeroRecords       : M.util.get_string('datatables_sZeroRecords', 'local_kopere_dashboard'),
+                    sEmptyTable        : M.util.get_string('datatables_sEmptyTable', "local_kopere_dashboard"),
+                    sInfo              : M.util.get_string('datatables_sInfo', "local_kopere_dashboard"),
+                    sInfoEmpty         : M.util.get_string('datatables_sInfoEmpty', "local_kopere_dashboard"),
+                    sInfoFiltered      : M.util.get_string('datatables_sInfoFiltered', "local_kopere_dashboard"),
+                    sInfoPostFix       : M.util.get_string('datatables_sInfoPostFix', "local_kopere_dashboard"),
+                    sInfoThousands     : M.util.get_string('datatables_sInfoThousands', "local_kopere_dashboard"),
+                    sLengthMenu        : M.util.get_string('datatables_sLengthMenu', "local_kopere_dashboard"),
+                    sLoadingRecords    : M.util.get_string('datatables_sLoadingRecords', "local_kopere_dashboard"),
+                    sProcessing        : M.util.get_string('datatables_sProcessing', "local_kopere_dashboard"),
+                    sErrorMessage      : M.util.get_string('datatables_sErrorMessage', "local_kopere_dashboard"),
+                    sZeroRecords       : M.util.get_string('datatables_sZeroRecords', "local_kopere_dashboard"),
                     sSearch            : "",
-                    sSearchPlaceholder : M.util.get_string('datatables_sSearch', 'local_kopere_dashboard'),
+                    sSearchPlaceholder : M.util.get_string('datatables_sSearch', "local_kopere_dashboard"),
                     buttons            : {
-                        print_text   : M.util.get_string('datatables_buttons_print_text', 'local_kopere_dashboard'),
-                        copy_text    : M.util.get_string('datatables_buttons_copy_text', 'local_kopere_dashboard'),
-                        csv_text     : M.util.get_string('datatables_buttons_csv_text', 'local_kopere_dashboard'),
-                        copySuccess1 : M.util.get_string('datatables_buttons_copySuccess1', 'local_kopere_dashboard'),
-                        copySuccess_ : M.util.get_string('datatables_buttons_copySuccess_', 'local_kopere_dashboard'),
-                        copyTitle    : M.util.get_string('datatables_buttons_copyTitle', 'local_kopere_dashboard'),
-                        copyKeys     : M.util.get_string('datatables_buttons_copyKeys', 'local_kopere_dashboard'),
+                        print_text   : M.util.get_string('datatables_buttons_print_text', "local_kopere_dashboard"),
+                        copy_text    : M.util.get_string('datatables_buttons_copy_text', "local_kopere_dashboard"),
+                        csv_text     : M.util.get_string('datatables_buttons_csv_text', "local_kopere_dashboard"),
+                        copySuccess1 : M.util.get_string('datatables_buttons_copySuccess1', "local_kopere_dashboard"),
+                        copySuccess_ : M.util.get_string('datatables_buttons_copySuccess_', "local_kopere_dashboard"),
+                        copyTitle    : M.util.get_string('datatables_buttons_copyTitle', "local_kopere_dashboard"),
+                        copyKeys     : M.util.get_string('datatables_buttons_copyKeys', "local_kopere_dashboard"),
                         pageLength   : {
-                            '_'  : M.util.get_string('datatables_buttons_pageLength_', 'local_kopere_dashboard'),
-                            '-1' : M.util.get_string('datatables_buttons_pageLength_1', 'local_kopere_dashboard'),
+                            '_'  : M.util.get_string('datatables_buttons_pageLength_', "local_kopere_dashboard"),
+                            '-1' : M.util.get_string('datatables_buttons_pageLength_1', "local_kopere_dashboard"),
                         }
                     },
                     select             : {
                         rows : {
-                            _ : M.util.get_string('datatables_buttons_select_rows_', 'local_kopere_dashboard'),
+                            _ : M.util.get_string('datatables_buttons_select_rows_', "local_kopere_dashboard"),
                             0 : "",
-                            1 : M.util.get_string('datatables_buttons_select_rows1', 'local_kopere_dashboard'),
+                            1 : M.util.get_string('datatables_buttons_select_rows1', "local_kopere_dashboard"),
                         }
                     },
                     oPaginate          : {
-                        sNext     : M.util.get_string('datatables_oPaginate_sNext', 'local_kopere_dashboard'),
-                        sPrevious : M.util.get_string('datatables_oPaginate_sPrevious', 'local_kopere_dashboard'),
-                        sFirst    : M.util.get_string('datatables_oPaginate_sFirst', 'local_kopere_dashboard'),
-                        sLast     : M.util.get_string('datatables_oPaginate_sLast', 'local_kopere_dashboard'),
+                        sNext     : M.util.get_string('datatables_oPaginate_sNext', "local_kopere_dashboard"),
+                        sPrevious : M.util.get_string('datatables_oPaginate_sPrevious', "local_kopere_dashboard"),
+                        sFirst    : M.util.get_string('datatables_oPaginate_sFirst', "local_kopere_dashboard"),
+                        sLast     : M.util.get_string('datatables_oPaginate_sLast', "local_kopere_dashboard"),
                     },
                     oAria              : {
-                        sSortAscending  : M.util.get_string('datatables_oAria_sSortAscending', 'local_kopere_dashboard'),
-                        sSortDescending : M.util.get_string('datatables_oAria_sSortDescending', 'local_kopere_dashboard'),
+                        sSortAscending  : M.util.get_string('datatables_oAria_sSortAscending', "local_kopere_dashboard"),
+                        sSortDescending : M.util.get_string('datatables_oAria_sSortDescending', "local_kopere_dashboard"),
                     }
                 };
                 params.responsive = true;
@@ -377,7 +377,7 @@ define([
                         'pageLength',
                         {
                             extend : 'print',
-                            text   : M.util.get_string('datatables_buttons_print_text', 'local_kopere_dashboard'),
+                            text   : M.util.get_string('datatables_buttons_print_text', "local_kopere_dashboard"),
                             title  : params.export_title
                         }, {
                             extend : 'pdf',
@@ -389,7 +389,7 @@ define([
                             title  : params.export_title
                         }, {
                             extend : 'csv',
-                            text   : M.util.get_string('datatables_buttons_csv_text', 'local_kopere_dashboard'),
+                            text   : M.util.get_string('datatables_buttons_csv_text', "local_kopere_dashboard"),
                             title  : params.export_title
                         }, {
                             extend : 'copy',
@@ -408,14 +408,14 @@ define([
                         setTimeout(function() {
                             _processing.show().html(
                                 "<div style='color:#e91e63'>" +
-                                M.util.get_string('datatables_sErrorMessage', 'local_kopere_dashboard', "<span class='counter'>30</span>") +
+                                M.util.get_string('datatables_sErrorMessage', "local_kopere_dashboard", "<span class='counter'>30</span>") +
                                 "</div>");
                         }, 500);
 
                         var timer = 30;
                         var _inteval = setInterval(function() {
                             if (--timer <= 0) {
-                                _processing.html(M.util.get_string('datatables_sProcessing', 'local_kopere_dashboard'));
+                                _processing.html(M.util.get_string('datatables_sProcessing', "local_kopere_dashboard"));
                                 clearInterval(_inteval);
                                 window[tableid].ajax.reload();
                             }
