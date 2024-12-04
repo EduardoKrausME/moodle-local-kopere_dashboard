2b0d877d9dc39c5ef7cdb606a64b94237fefdd09
Eduardo Kraus <kraus@eduardokraus.com>
2024-12-04 17:24:22 -0300

    V 2.7.5

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024120400;
+$plugin->version = 2024120401;
 $plugin->requires = 2015111610;
-$plugin->release = '2.7.4';
+$plugin->release = '2.7.5';
 $plugin->maturity = MATURITY_RC;
 $plugin->component = "local_kopere_dashboard";
