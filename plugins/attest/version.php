d4dba5f3a6fd4d7c22496ace395bc8b44517c979
Eduardo Kraus <kraus@eduardokraus.com>
2026-06-18 11:58:04 -0300

    attest 1.0.1

--- plugins/attest/version.php
+++ plugins/attest/version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2026051002;
-$plugin->release = "1.0.0";
+$plugin->version = 2026061800;
+$plugin->release = "1.0.1";
 $plugin->component = "koperedashboard_attest";
 $plugin->requires = 2024042200;
 $plugin->maturity = MATURITY_STABLE;
